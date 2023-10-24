85f8df49aaa391c56fc2062ee10c884abede7ff0
Andrew Rutter <andlrutt@vols.utk.edu>
2023-10-24 16:11:23 -0400

    create form submission

---
+++ apps/form-builder/src/server/actions/formSubmissions.ts
@@ -0,0 +1,28 @@
+import dbConnect from "@/utils/db-connect";
+import { CreateFormSubmissionRequest, FormResponse, FormSchema, FormSubmission, FormSubmissionResponse, FormSubmissionSchema } from "@hack4impact-utk/internal-models";
+import { getFormById } from "./forms";
+
+
+export async function createFormSubmission(formId: string, formSubmission: CreateFormSubmissionRequest) {
+    await dbConnect()
+
+    const createdFormSubmission: FormSubmission = await FormSubmissionSchema.create(formSubmission)
+
+    const castedFormSubmission = createdFormSubmission as FormSubmissionResponse
+
+  const form: FormResponse | null = await getFormById(formId);
+
+  const formSubmissionId = castedFormSubmission._id
+
+  const existingSubmissionIds = form?.submissions.map((q) => q._id);
+
+  if (form != null) {
+    const res: FormResponse | null = await FormSchema.findByIdAndUpdate(
+      { _id: formId },
+      { submissions: existingSubmissionIds?.concat(formSubmissionId) }
+    );
+    return res;
+  }
+  return null;
+
+}
