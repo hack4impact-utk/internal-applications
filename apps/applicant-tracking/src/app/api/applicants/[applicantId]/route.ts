bee95d75d70443bb8d4c9120614928a455eec981
mpatrikios <mpatrikios@icloud.com>
2023-10-30 15:50:09 -0400

    created delete api endpoint function

---
+++ apps/applicant-tracking/src/app/api/applicants/[applicantId]/route.ts
@@ -0,0 +1,24 @@
+import dbConnect from "@/utils/db-connect";
+import { deleteApplicant, zObjectId } from "@hack4impact-utk/internal-models";
+import { NextRequest, NextResponse } from "next/server";
+
+// @route DELETE /api/applicants/[applicantId] - Deletes an applicant
+export async function DELETE (request: NextRequest, 
+    { params }: { params: { applicantId: string } }) 
+{
+
+await dbConnect();
+
+const applicantId = params.applicantId
+
+const validationResult = zObjectId.safeParse(applicantId)
+
+if(!validationResult.success){
+    return NextResponse.json({message: "Invalid ID"}, { status: 400 })
+}
+
+await deleteApplicant(applicantId)
+return new NextResponse(undefined, { status: 204 })
+
+
+}
