f08eb8300d53ad7447ecbbe7139c8ef68991f5ee
Braden Wingfield <wingfieldbraden@gmail.com>
2023-10-30 19:15:41 -0400

    New changes

---
+++ apps/form-builder/src/app/api/forms/[formId]/route.ts
@@ -0,0 +1,19 @@
+import { deleteForm } from "@/server/actions/forms"
+import { zObjectId } from "@hack4impact-utk/internal-models"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function DELETE(
+    _request: NextRequest,
+    { params }: { params: { formId: string } })
+{
+    const formId = params.formId
+    const validationResult = zObjectId.safeParse(formId)
+    if (!validationResult.success) {
+        return NextResponse.json ({message: "invalid id"}, { status: 400 })
+    }
+    const response = await deleteForm(formId)
+    if (response === null) {
+        return NextResponse.json ({message: "form not found"}, { status: 404 })
+    }
+    return new NextResponse(undefined, { status: 204 })
+}
