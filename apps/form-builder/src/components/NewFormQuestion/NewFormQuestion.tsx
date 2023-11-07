fba869fc19d0ea5e180dd5fc364d05274c74ae06
Amy Huang <ahuang16@vols.utk.edu>
2023-11-06 22:38:49 -0500

    Adds a component

---
+++ apps/form-builder/src/components/NewFormQuestion/NewFormQuestion.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { Button, Checkbox, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material"
+
+export default function NewFormQuestion() {
+    return (
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <TextField 
+                id="formId" 
+                label="Form ID" 
+                required
+            />
+            <TextField 
+                id="title" 
+                label="Title" 
+            /> 
+            <TextField 
+                id="description" 
+                label="Description" 
+                multiline 
+            />
+            <FormControlLabel control={<Checkbox />} label="Required" required/>
+            <FormControl required>
+                <FormLabel id="question-type">Question Type</FormLabel>
+                <RadioGroup>
+                    <FormControlLabel value="numeric" control={<Radio/>} label="Numeric" />
+                    <FormControlLabel value="text" control={<Radio/>} label="Text" />
+                    <FormControlLabel value="file-upload" control={<Radio />} label="File Upload" />
+                    <FormControlLabel value="multiple-choice" control={<Radio/>} label="Multiple Choice" />
+                </RadioGroup>
+            </FormControl> 
+            <Button type="submit" variant="outlined">Submit</Button>  
+        </div>
+    ); 
+}
