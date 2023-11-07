c4dbd12acf43d556d50eea814a557cead6c6fb39
Annalise Smith <annaliseismith@gmail.com>
2023-11-07 14:45:46 -0500

    getAllFormSubmissions changed after pr

--- apps/form-builder/src/app/api/forms/[formId]/formSubmissions/route.ts
+++ apps/form-builder/src/app/api/forms/[formId]/formSubmissions/route.ts
@@ -4,9 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { zObjectId } from '@hack4impact-utk/internal-models';
 
 export async function GET (_request: NextRequest, { params }: { params: { formId: string } }) {
-    // if formId is valid, get form submissions and return them with status code 200
+    // if formId is valid, get form submissions and return them with status code 200, if formSubmissions is null return status code 404 with error message
     if(zObjectId.safeParse(params.formId).success) {
-        const formSubmissions = getFormSubmissions(params.formId)
+        const formSubmissions = await getFormSubmissions(params.formId)
+        if (formSubmissions == null)
+            return NextResponse.json({message: "form not found"}, {status: 404})
         return NextResponse.json(formSubmissions, { status: 200 })
     }
 }
