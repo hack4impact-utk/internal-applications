8a455ae25c32a3ce224360caf4bf2572dc303a64
Ahmad Tobasei <ahmadtobasei@Ahmads-MacBook-Air.local>
2023-11-07 22:18:50 -0500

    i hope im pushing this right

--- packages/internal-models/src/db/actions/Applicant.ts
+++ packages/internal-models/src/db/actions/Applicant.ts
@@ -1,5 +1,10 @@
 import ApplicantSchema from '@/db/models/Applicant';
 
+export async function getApplicants() { 
+  const applicant = await ApplicantSchema.find();
+  return applicant;
+ }
+
 export async function deleteApplicant(id: string) {
   await ApplicantSchema.findByIdAndDelete(id);
 }
