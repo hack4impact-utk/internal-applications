f0972a851da701e0e98cb7abbd808b7380d9d988
Zavier Miller <zavierjmiller@gmail.com>
2023-11-08 21:12:13 -0500

    wip

--- apps/applicant-tracking/src/services/index.ts
+++ apps/applicant-tracking/src/services/index.ts
@@ -1,3 +1,5 @@
 import ApplicantApiService from "./api/applicant";
+import { CalendlyApiService } from "./api/calendly";
 
 export const ApplicantService = new ApplicantApiService()
+export const CalendlyService = new CalendlyApiService(process.env.CALENDLY_PAT)
--- apps/applicant-tracking/src/types/global.d.ts
+++ apps/applicant-tracking/src/types/global.d.ts
@@ -8,6 +8,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGODB_URI: string // the URI of the mongodb instance to connect to
+      CALENDLY_PAT: string // personal access token for Calendly integration
+      CALENDLY_ORG_URI: string // URI of the H4I UTK calendly org
+      CALENDLY_USER_URI: string // URI of the H4I UTK calendly user 
+      CALENDLY_SIGNING_SECRET: string // shared signing secret used to verify origin of webhook requests
     }
   }
 }
