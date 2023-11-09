10fffdd10a2cb56d5f824deed0a9766dadba7197
Zavier Miller <zavierjmiller@gmail.com>
2023-11-08 20:27:44 -0500

    revert route.ts change

--- apps/form-builder/src/app/api/forms/route.ts
+++ apps/form-builder/src/app/api/forms/route.ts
@@ -1,18 +1,22 @@
-import { createForm } from '@/server/actions/forms';
-import { zCreateFormRequest } from '@hack4impact-utk/internal-models';
-import { NextRequest, NextResponse } from 'next/server';
+import { createForm } from "@/server/actions/forms";
+import { zCreateFormRequest } from "@hack4impact-utk/internal-models";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  const requestBody = await request.json();
-  const validationResult = zCreateFormRequest.safeParse(requestBody);
-  if (validationResult.success) {
-    const form = await createForm(requestBody);
+export async function POST (request: NextRequest) { 
 
-    return NextResponse.json({ _id: form._id }, { status: 201 });
-  } else {
-    return NextResponse.json(
-      { message: 'Input was not in correct format' },
-      { status: 400 }
-    );
-  }
+    
+    const requestBody = await request.json();
+    const validationResult = zCreateFormRequest.safeParse(requestBody);
+    if (validationResult.success) {
+        const form = await createForm(requestBody);
+        
+        
+        return NextResponse.json({_id:form._id}, {status: 201})
+    }
+    else {
+        return NextResponse.json({message: "Input was not in correct format"}, {status: 400})
+    }
+
+
+
 }
