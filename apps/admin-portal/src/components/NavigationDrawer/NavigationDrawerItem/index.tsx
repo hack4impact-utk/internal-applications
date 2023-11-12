1e42ce4af98f434fd80d36a18c33b1d8074671fb
mikjmcc26 <mmccor23@vols.utk.edu>
2023-11-12 13:33:29 -0500

    Formatting and commenting

--- apps/admin-portal/src/components/NavigationDrawer/NavigationDrawerItem/index.tsx
+++ apps/admin-portal/src/components/NavigationDrawer/NavigationDrawerItem/index.tsx
@@ -2,7 +2,7 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
 import Link from 'next/link';
 
-// Not using usePathname(), waiting to add icon
+// Props for list 
 interface ListProps {
    text: string;
    route: string;
@@ -10,14 +10,14 @@ interface ListProps {
 
 export default function NavigationDrawerItem({ text, route }: ListProps) {
    return (
-      <ListItemButton 
-         component={Link} 
-         href={route} 
+      <ListItemButton
+         component={Link}
+         href={route}
       >
          <ListItemText
-           primary={text}
-           primaryTypographyProps={{ variant: 'body1' }}
-         /> 
+            primary={text}
+            primaryTypographyProps={{ variant: 'body1' }}
+         />
       </ListItemButton>
    );
 }
--- apps/admin-portal/src/components/NavigationDrawer/index.tsx
+++ apps/admin-portal/src/components/NavigationDrawer/index.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
-// import { isOpen, useState};
+// MUI 
 import Drawer from '@mui/material/Drawer';
 import { Box } from '@mui/system'
-// import Collapse from '@mui/material/Collapse'
 import NavigationDrawerItem from './NavigationDrawerItem';
 
-export default function NavigationDrawer () {
+export default function NavigationDrawer() {
    const drawerWidth = 280;
 
+   // Setting up Drawer
    return (
-     <Drawer
-       variant="permanent"
-       sx={{
-         width: drawerWidth,
-         flexShrink: 0,
-         '& .MuiDrawer-paper': {
+      <Drawer
+         variant="permanent"
+         sx={{
             width: drawerWidth,
-            overflowX: 'hidden',
-            boxSizing: 'border-box',
-         },
-       }}
-       open={true}
-     >
-      <Box sx={{ width: drawerWidth }}>
-         <Box
-            sx={{
-               display: 'flex',
-               flexDirection: 'column',
-               px: 2,
-               pt: 2,
-               justifyContent: 'center'
-            }}
-         >
-            <NavigationDrawerItem text="Inbox" route="/inbox" />
-            <NavigationDrawerItem text="Dashboard" route="/dashboard" />
-            <NavigationDrawerItem text="Categories" route="/categories" />
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+               width: drawerWidth,
+               overflowX: 'hidden',
+               boxSizing: 'border-box',
+            },
+         }}
+         open={true}
+      >
+         <Box sx={{ width: drawerWidth }}>
+            <Box
+               sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  px: 2,
+                  pt: 2,
+                  justifyContent: 'center'
+               }}
+            >
+               {/* What is being displayed */}
+               <NavigationDrawerItem text="Inbox" route="/inbox" />
+               <NavigationDrawerItem text="Dashboard" route="/dashboard" />
+               <NavigationDrawerItem text="Categories" route="/categories" />
+            </Box>
          </Box>
-      </Box>
-     </Drawer>
+      </Drawer>
    );
 }
