d511c8f43c6b3b266c8f44640945505611482187
Zavier Miller <zavierjmiller@gmail.com>
2023-11-15 18:12:23 -0500

    finish

--- apps/applicant-tracking/src/app/api/applicants/[applicantId]/route.ts
+++ apps/applicant-tracking/src/app/api/applicants/[applicantId]/route.ts
@@ -1,45 +1,48 @@
-import dbConnect from "@/utils/db-connect";
-import { deleteApplicant, zObjectId } from "@hack4impact-utk/internal-models";
-import { NextRequest, NextResponse } from "next/server";
+import dbConnect from '@/utils/db-connect';
+import {
+  deleteApplicant,
+  getApplicantById,
+  zObjectId,
+} from '@hack4impact-utk/internal-models';
+import { NextRequest, NextResponse } from 'next/server';
 
 // @route DELETE /api/applicants/[applicantId] - Deletes an applicant
-export async function DELETE (request: NextRequest, 
-    { params }: { params: { applicantId: string } }) 
-{
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { applicantId: string } }
+) {
+  await dbConnect();
 
-await dbConnect();
+  const applicantId = params.applicantId;
 
-const applicantId = params.applicantId
+  const validationResult = zObjectId.safeParse(applicantId);
 
-const validationResult = zObjectId.safeParse(applicantId)
+  if (!validationResult.success) {
+    return NextResponse.json({ message: 'Invalid ID' }, { status: 400 });
+  }
 
-if(!validationResult.success){
-    return NextResponse.json({message: "Invalid ID"}, { status: 400 })
+  await deleteApplicant(applicantId);
+  return new NextResponse(undefined, { status: 204 });
 }
 
-await deleteApplicant(applicantId)
-return new NextResponse(undefined, { status: 204 })
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: { applicantId: string } }
+) {
+  await dbConnect();
 
-}
+  const applicantId = params.applicantId;
 
-export async function GET (_request: NextRequest, 
-    { params }: { params: { applicantId: string } }) { 
+  const validationResult = zObjectId.safeParse(applicantId);
 
-await dbConnect();
-
-const applicantId = params.applicantId;
-
-const validationResult = zObjectId.safeParse(applicantId); 
-
-if (!validationResult.success) {
+  if (!validationResult.success) {
     return new Response('Invalid applicant ID', { status: 400 });
-}
+  }
 
-const form = await zObjectId.getApplicantById(applicantId);
-if (!form) {
-    return new NextResponse('Form not found', { status: 404 })
-}
+  const applicant = await getApplicantById(applicantId);
+  if (!applicant) {
+    return new NextResponse('Applicant not found', { status: 404 });
+  }
 
-return NextResponse.json(form, { status: 200 })
-    
+  return NextResponse.json(applicant, { status: 200 });
 }
