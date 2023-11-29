02ccbf32d15bcbb85f221edf00d5996fe38ba0da
wessoms <sessoms.will@gmail.com>
2023-11-29 02:00:21 -0500

    Added extra member props and began styling member component

--- apps/admin-portal/src/components/Member/index.tsx
+++ apps/admin-portal/src/components/Member/index.tsx
@@ -1,15 +1,39 @@
+import { Box, Typography } from '@mui/material';
+
 interface memberParams {
-    name: string;
-    year: number;
-    project: string;
+  email: string;
+  name: string;
+  project: string;
+  gradClass: string;
+  pronouns?: string;
+  major: string;
+  role: string;
+  github: string;
+  linkedin: string;
 }
 
 export default function Member(params: memberParams) {
-    return (
-        <div>
-            <h1>{params.name}</h1>
-            <h1>{params.year}</h1>
-            <h2>{params.project}</h2>
-        </div>
-    );
+  return (
+    <Box
+      sx={{
+        border: 'solid',
+        maxWidth: 400,
+        textAlign: 'center',
+        backgroundColor: 'PaleTurquoise',
+      }}
+    >
+      <Typography variant="h5" fontFamily={'arial'}>
+        Name: {params.name} <br />
+        Email: {params.email} <br />
+        Project: {params.project} <br />
+        Grad Class: {params.gradClass} <br />
+        {params.pronouns ? 'Pronouns: ' + params.pronouns : ''}
+        {params.pronouns ? <br /> : ''}
+        Major: {params.major} <br />
+        Role: {params.role} <br />
+        Github: {params.github} <br />
+        LinkedIn:{params.linkedin}
+      </Typography>
+    </Box>
+  );
 }
