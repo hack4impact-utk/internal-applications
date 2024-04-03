c22a1f5ca437ea783fa79761b259e87df60b9bea
John Cheek <cheekjohnm@gmail.com>
2024-04-03 17:31:03 -0400

    Finished the TextQuestionAnalytics component

--- apps/form-builder/src/components/FormSubmissions/TextQuestionAnalytics.tsx
+++ apps/form-builder/src/components/FormSubmissions/TextQuestionAnalytics.tsx
@@ -1,34 +1,50 @@
-import { FormQuestionResponse, FormSubmissionResponse } from "@hack4impact-utk/internal-models";
-import { List, ListItem, ListItemText } from "@mui/material";
+import {
+  FormQuestionResponse,
+  FormSubmissionResponse,
+} from '@hack4impact-utk/internal-models';
+import * as React from 'react';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemContent from '@mui/joy/ListItemContent';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Sheet from '@mui/joy/sheet';
 
-
-interface Props{
-    question: FormQuestionResponse,
-    responses: FormSubmissionResponse[]
+interface Props {
+  question: FormQuestionResponse;
+  responses: FormSubmissionResponse[];
 }
 
-export default function TextQuestionAnalytics({question, responses}: Props){
-    console.log(question)
-    return (
-        <List
-        sx = {{
-            width: "100%",
-            maxWidth: 360,
-            bgcolor: "background.paper",
-            position: "relative",
-            overflow: "auto",
-            maxHeight: 300,
-            "& ul": { padding: 0 }
-        }}
-        subheader = {<li/>}
-        >
-            <ListItem>
-                <ListItemText>
-                    <ul>
-                        {responses.map(item => <li>{item.questionResponses[1].answer}</li>)} 
-                    </ul>
-                </ListItemText>
-        </ListItem>
-    </List>
+export default function TextQuestionAnalytics({ question, responses }: Props) {
+  function getSubmission() {
+    let array = [];
+    for (let i = 0; i < responses.length; i++) {
+      let num = responses[i].questionResponses.findIndex(
+        (element) => element.question._id.toString() === question._id.toString()
+      );
+      array.push(responses[i].questionResponses[num].answer);
+    }
+    return array;
+  }
+
+  return (
+    <Sheet
+      variant="outlined"
+      sx={{
+        width: 400,
+        maxHeight: 300,
+        overflow: 'auto',
+        borderRadius: 'sm',
+      }}
+    >
+      <List>
+        {getSubmission().map((submission, index) => (
+          <ListItem key={index}>
+            <ListItemButton selected={false} variant="soft">
+              <ListItemContent>{submission}</ListItemContent>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Sheet>
   );
 }
