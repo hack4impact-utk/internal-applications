9be9214b73c8e17be1bff82a81b1e3cc104ef3c9
Zavier Miller <zavierjmiller@gmail.com>
2024-04-12 16:59:09 -0400

    revert testing code

--- apps/applicant-tracking/src/app/page.tsx
+++ apps/applicant-tracking/src/app/page.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-import TermSelector from '@/components/TermSelector';
-
 export default function DashboardPage() {
-  //  return <h1>Dashboard page</h1>;
-  return (
-    <div>
-      <TermSelector />
-    </div>
-  );
+  return <h1>Dashboard page</h1>;
 }
