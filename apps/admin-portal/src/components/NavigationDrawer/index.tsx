a231d5fd02575b8259125a50f5e33c657d90597a
mikjmcc26 <mmccor23@vols.utk.edu>
2024-04-15 15:23:12 -0400

    169 - Side bar only show on desktop

--- apps/admin-portal/src/components/NavigationDrawer/index.tsx
+++ apps/admin-portal/src/components/NavigationDrawer/index.tsx
@@ -1,11 +1,20 @@
+'use client';
 import React from 'react';
 // MUI
 import Drawer from '@mui/material/Drawer';
 import { Box } from '@mui/system';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import NavigationDrawerItem from './NavigationDrawerItem';
 
 export default function NavigationDrawer() {
+
   const drawerWidth = 280;
+  const isDesktop = useMediaQuery('(min-width:768px)');
+
+  // Make sure nav drawer only shows on Desktop
+  if (!isDesktop) {
+    return null;
+  }
 
   // Setting up Drawer
   return (
