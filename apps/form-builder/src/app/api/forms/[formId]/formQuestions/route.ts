b9208d8d079eede48b0fe53eb9c92c5a1b722bf6
Ian Innes <ianinnes502@gmail.com>
2024-04-16 16:24:32 -0400

    api component

--- apps/form-builder/src/app/api/forms/[formId]/formQuestions/route.ts
+++ apps/form-builder/src/app/api/forms/[formId]/formQuestions/route.ts
@@ -1,6 +1,17 @@
-import { getFormQuestions } from '@/server/actions/formQuestions';
-import { zObjectId } from '@hack4impact-utk/internal-models';
+import NewFormQuestion from '@/components/NewFormQuestion/NewFormQuestion';
+import {
+  createFormQuestions,
+  getFormQuestions,
+} from '@/server/actions/formQuestions';
+import { createForm } from '@/server/actions/forms';
+import dbConnect from '@/utils/db-connect';
+import {
+  zCreateFormQuestionRequest,
+  zObjectId,
+} from '@hack4impact-utk/internal-models';
+import { requestToBodyStream } from 'next/dist/server/body-streams';
 import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
 export async function GET(
   _request: NextRequest,
@@ -16,4 +27,25 @@ export async function GET(
   } else {
     return NextResponse.json({ message: 'Invalid formId' }, { status: 400 });
   }
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { formId: string } }
+) {
+  const formId = params.formId;
+  await dbConnect();
+  const requestValue = await request.json();
+
+  if (!z.array(zCreateFormQuestionRequest).safeParse(requestValue)) {
+    return NextResponse.json({}, { status: 400 });
+  }
+
+  if (!zObjectId.safeParse(formId)) {
+    return NextResponse.json({}, { status: 404 });
+  }
+
+  createFormQuestions(formId, requestValue);
+
+  return NextResponse.json({}, { status: 201 });
 }
