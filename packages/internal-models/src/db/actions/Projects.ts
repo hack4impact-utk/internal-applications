3dee8f3a6e384c3ba4c1f020ad3faf92e9b871d9
mikjmcc26 <mmccor23@vols.utk.edu>
2024-04-16 13:28:50 -0400

    Updating import line

--- packages/internal-models/src/db/actions/Projects.ts
+++ packages/internal-models/src/db/actions/Projects.ts
@@ -1,4 +1,6 @@
-import { ProjectSchema } from '../models';
+// Server function to retrieve all projects from database
+
+import ProjectSchema from '@/db/models/Project';
 
 export async function getAllProjects() {
   const projects = await ProjectSchema.find({});
