5b44c4efa60036ec331557c634b0b94a0a85366b
Laura Smith <113145914+LSmith2174@users.noreply.github.com>
2024-04-16 13:54:15 -0400

    added Get all forms api endpoint

--- apps/form-builder/src/app/api/forms/route.ts
+++ apps/form-builder/src/app/api/forms/route.ts
@@ -1,6 +1,12 @@
 import { createForm } from '@/server/actions/forms';
 import { zCreateFormRequest } from '@hack4impact-utk/internal-models';
 import { NextRequest, NextResponse } from 'next/server';
+import { getAllForms } from '@/server/actions/forms';
+
+export async function GET(): Promise<NextResponse> {
+  const forms = await getAllForms();
+  return NextResponse.json(forms, { status: 200 });
+}
 
 export async function POST(request: NextRequest) {
   const requestBody = await request.json();
