0d282a8f014ca244dc4675fe3c72d626400b7e87
smohyud1 <smohyud1@vols.utk.edu>
2024-04-19 01:17:41 -0400

    Refactor ApplicantListItem component to use props instead of direct parameter access and fixed styling

--- apps/applicant-tracking/src/components/ApplicantListItem/index.tsx
+++ apps/applicant-tracking/src/components/ApplicantListItem/index.tsx
@@ -2,28 +2,28 @@ import { ApplicantResponse } from '@hack4impact-utk/internal-models';
 import { Chip, TableCell, TableRow } from '@mui/material';
 import LaunchIcon from '@mui/icons-material/Launch';
 import Button from '@mui/material/Button';
-
+interface ApplicantListItemProps {
+  applicant: ApplicantResponse;
+}
 //Each row should display the applicant name (first and last), status, NetID and application
-export default function ApplicantListItem(applicant: ApplicantResponse) {
+export default function ApplicantListItem(props: ApplicantListItemProps) {
   return (
     <TableRow>
-      <TableCell align={'left'} style={{ top: 57, minWidth: 170 }}>
-        {applicant.firstName} {applicant.lastName}
+      <TableCell align={'left'}>
+        {props.applicant.firstName} {props.applicant.lastName}
       </TableCell>
-      <TableCell align={'left'} style={{ top: 57, minWidth: 170 }}>
-        <Chip label={applicant.status} />
+      <TableCell align={'left'}>
+        <Chip label={props.applicant.status} />
       </TableCell>
-      <TableCell align={'left'} style={{ top: 57, minWidth: 170 }}>
-        {applicant.netid}
-      </TableCell>
-      <TableCell align={'left'} style={{ top: 57, minWidth: 170 }}>
+      <TableCell align={'left'}>{props.applicant.netid}</TableCell>
+      <TableCell align={'left'}>
         <Button
           variant="outlined"
-          style={{ color: 'black', borderColor: 'black' }}
+          color="inherit"
           size="small"
+          endIcon={<LaunchIcon />}
         >
           View Application
-          <LaunchIcon />
         </Button>
       </TableCell>
     </TableRow>
