8aceccf929cc938f0483df5152f56507a392c647
Laura Smith <113145914+LSmith2174@users.noreply.github.com>
2024-04-20 21:44:35 -0400

    created form table component then added it to forms page.

--- apps/form-builder/src/app/forms/page.tsx
+++ apps/form-builder/src/app/forms/page.tsx
@@ -1,7 +1,16 @@
-export default function Forms() {
+import FormsTable from '@/components/FormsTable';
+import { getAllForms } from '@/server/actions/forms';
+import { Box, Typography } from '@mui/material';
+
+export default async function Forms() {
+  const forms = JSON.parse(JSON.stringify(await getAllForms()));
+
   return (
-    <div>
-      <p>forms will go here, hopefully!</p>
-    </div>
+    <Box>
+      <Typography variant="h2">Forms</Typography>
+      <Box minHeight={400} width={'80%'} pt={4}>
+        <FormsTable forms={forms}></FormsTable>
+      </Box>
+    </Box>
   );
 }
