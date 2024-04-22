8f383564fe59fb5467140e323aa09c4c3bd728aa
Logan Draper <logan050531@gmail.com>
2024-04-22 19:26:58 -0400

    formatted

--- apps/form-builder/src/components/NumericOptions.tsx
+++ apps/form-builder/src/components/NumericOptions.tsx
@@ -1,23 +1,17 @@
 import * as React from 'react';
 import Switch from '@mui/material/Switch';
-import { FormControlLabel, TextField} from '@mui/material';
+import { FormControlLabel, TextField } from '@mui/material';
 
-export default function NumericOptions(){
-return (
-<>
+export default function NumericOptions() {
+  return (
+    <>
+      <FormControlLabel
+        control={<Switch defaultChecked />}
+        label="Allow Decimals?"
+      />
 
-<FormControlLabel
-control  = {<Switch defaultChecked />} label = "Allow Decimals?"
-    />
-
-<TextField
-id="Min-Val" label="Minimum Value" variant="outlined"
-/>
-<TextField 
-id="Max-Val" label="Maximum Value" variant="outlined" />
-</>
-)
+      <TextField id="Min-Val" label="Minimum Value" variant="outlined" />
+      <TextField id="Max-Val" label="Maximum Value" variant="outlined" />
+    </>
+  );
 }
-
-
-
