c2a8431765f2650f99701412791fe2ff71b931ef
Braden Wingfield <wingfieldbraden@gmail.com>
2024-04-23 19:32:04 -0400

    Create a BarGraphAnalytics component. Any "other" responses to a multiple choice question are displayed in a list.

--- apps/form-builder/src/components/MultipleChoiceQuestionAnalytics/Index.tsx
+++ apps/form-builder/src/components/MultipleChoiceQuestionAnalytics/Index.tsx
@@ -2,46 +2,79 @@ import {
   FormQuestionResponse,
   FormSubmissionResponse,
 } from '@hack4impact-utk/internal-models';
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 
-//Takes in the question and response as props
+// Takes in the question and response as props
 interface Props {
   question: FormQuestionResponse;
   responses: FormSubmissionResponse[];
 }
 
-//function that displays the list
+// Function that displays the list
 export default function MultipleChoiceQuestionAnalytics({
   question,
   responses,
 }: Props) {
-  //gets the specific submission to a question from each response from a single form
+  // Gets the specific submission to a question from each response from a single form
   function getAnswers() {
     let array = [];
-    //for a given form, the findIndex function will find the specific question from the list of questionResponses
+    // For a given form, the findIndex function will find the specific question from the list of questionResponses
     for (let i = 0; i < responses.length; i++) {
       let num = responses[i].questionResponses.findIndex(
         (element) => element.question._id.toString() === question._id.toString()
       );
-      if (num == -1) {
+      if (num === -1) {
         continue;
       } else {
-        array.push(responses[i].questionResponses[num].answer);
+        // Get the answer and options
+        let answer = responses[i].questionResponses[num].answer;
+        let options =
+          responses[i].questionResponses[num].question.multipleChoiceOptions
+            ?.options;
+
+        // Check if the answer is not in the options array
+        let answerInOptions = false;
+        for (let j = 0; j < options!.length; j++) {
+          if (options![j] === answer) {
+            answerInOptions = true;
+            break;
+          }
+        }
+
+        // If the answer is not in the options, add it to the array
+        if (!answerInOptions) {
+          array.push(answer);
+        }
       }
     }
     return array;
   }
 
-  //displays the submissions in a list format
+  // Displays the submissions in a list format with scroll functionality
   return (
-    <List>
-      {getAnswers().map((submission, index) => (
-        <ListItem key={index}>
-          <ListItemButton>
-            <ListItemText>{submission}</ListItemText>
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Typography variant="h6">
+        {question.questionType}
+        {" 'Other' responses"}
+      </Typography>
+      <Box sx={{ maxHeight: 300, overflow: 'auto' }}>
+        <List>
+          {getAnswers().map((submission, index) => (
+            <ListItem key={index}>
+              <ListItemButton>
+                <ListItemText>{submission}</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </>
   );
 }
