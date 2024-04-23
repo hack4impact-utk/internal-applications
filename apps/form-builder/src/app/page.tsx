56cc02a0be7c13e33ce7a9592854d26eb3781dd0
Braden Wingfield <wingfieldbraden@gmail.com>
2024-04-23 15:04:09 -0400

    Currrently displays a list of the all answers. Need to add functionality for "Other" answers in a scrollable list.

--- apps/form-builder/src/app/page.tsx
+++ apps/form-builder/src/app/page.tsx
@@ -1,3 +1,87 @@
+import React from 'react';
+import {
+  FormQuestionResponse,
+  FormSubmissionResponse,
+} from '@hack4impact-utk/internal-models';
+import MultipleChoiceQuestionAnalytics from '@/components/MultipleChoiceQuestionAnalytics/Index';
+
+// Detailed mock data for the FormQuestionResponse
+const mockQuestion: FormQuestionResponse = {
+  _id: '656ea21e70fac31f8c4aab0f',
+  title: 'Which design tools are you proficient in?',
+  isRequired: true,
+  questionType: 'MultipleChoice',
+  multipleChoiceOptions: {
+    options: [
+      'Adobe Photoshop',
+      'Adobe Illustrator',
+      'Figma',
+      'Sketch',
+      'InVision',
+    ],
+    allowOther: true,
+    choiceType: 'Multiple',
+  },
+  createdAt: new Date('2023-12-05T04:07:58.579Z'),
+  updatedAt: new Date('2023-12-05T04:07:58.579Z'),
+};
+
+// Correctly structured mock data for FormSubmissionResponse[]
+const mockResponses: FormSubmissionResponse[] = [
+  {
+    _id: 'response1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    questionResponses: [
+      {
+        question: { ...mockQuestion },
+        answer: 'Adobe Photoshop',
+      },
+    ],
+  },
+  {
+    _id: 'response2',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    questionResponses: [
+      {
+        question: { ...mockQuestion },
+        answer: 'Adobe Illustrator',
+      },
+    ],
+  },
+  {
+    _id: 'response3',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    questionResponses: [
+      {
+        question: { ...mockQuestion },
+        answer: 'Random',
+      },
+    ],
+  },
+  {
+    _id: 'response3',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    questionResponses: [
+      {
+        question: { ...mockQuestion },
+        answer: 'Burger',
+      },
+    ],
+  },
+];
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <MultipleChoiceQuestionAnalytics
+        question={mockQuestion}
+        responses={mockResponses}
+      />
+    </div>
+  );
 }
--- apps/form-builder/src/components/MultipleChoiceQuestionAnalytics/Index.tsx
+++ apps/form-builder/src/components/MultipleChoiceQuestionAnalytics/Index.tsx
@@ -1,55 +1,47 @@
-'use client';
-import * as React from 'react';
 import {
   FormQuestionResponse,
   FormSubmissionResponse,
 } from '@hack4impact-utk/internal-models';
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 
+//Takes in the question and response as props
 interface Props {
   question: FormQuestionResponse;
   responses: FormSubmissionResponse[];
 }
 
-export default function MultipleChoiceQuetionAnalytics({
+//function that displays the list
+export default function MultipleChoiceQuestionAnalytics({
   question,
   responses,
 }: Props) {
-  // Render function for the scrollable list
-  function renderRow(props: ListChildComponentProps) {
-    return (
-      <ListItem style={} key={} component="div" disablePadding>
-        <ListItemButton>
-          <ListItemText primary={} />
-        </ListItemButton>
-      </ListItem>
-    );
+  //gets the specific submission to a question from each response from a single form
+  function getAnswers() {
+    let array = [];
+    //for a given form, the findIndex function will find the specific question from the list of questionResponses
+    for (let i = 0; i < responses.length; i++) {
+      let num = responses[i].questionResponses.findIndex(
+        (element) => element.question._id.toString() === question._id.toString()
+      );
+      if (num == -1) {
+        continue;
+      } else {
+        array.push(responses[i].questionResponses[num].answer);
+      }
+    }
+    return array;
   }
+
+  //displays the submissions in a list format
   return (
-    <Box
-      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <Box
-        sx={{
-          width: '100%',
-          height: 400,
-          maxWidth: 360,
-        }}
-      >
-        <FixedSizeList
-          height={400}
-          width={360}
-          itemSize={46}
-          itemCount={200} // Update itemCount to match the number of items to display in the scrollable list
-          overscanCount={5}
-        >
-          {renderRow}
-        </FixedSizeList>
-      </Box>
-    </Box>
+    <List>
+      {getAnswers().map((submission, index) => (
+        <ListItem key={index}>
+          <ListItemButton>
+            <ListItemText>{submission}</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 }
