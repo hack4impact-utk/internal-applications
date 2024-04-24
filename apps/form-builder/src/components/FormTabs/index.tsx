62020c3c562b8ac4bab2e0ea03b8e07b899290dd
LSmith2174 <113145914+LSmith2174@users.noreply.github.com>
2024-04-24 01:21:48 -0400

    added submissions table to submissions tab on /forms/[formId] page

--- apps/form-builder/src/components/FormTabs/index.tsx
+++ apps/form-builder/src/components/FormTabs/index.tsx
@@ -1,8 +1,47 @@
 'use client';
-import { Box, Tabs, Tab, Button } from '@mui/material';
+import { Box, Tabs, Tab, Button, Typography } from '@mui/material';
 import React from 'react';
+import FormSubmissionTable from '../FormSubmissions/FormSubmissionTable';
+import { FormResponse } from '@hack4impact-utk/internal-models';
 
-export default function FormTabs() {
+interface formsProp {
+  form: FormResponse;
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function FormTabs(props: formsProp) {
   const [value, setValue] = React.useState(0);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -17,12 +56,20 @@ export default function FormTabs() {
             onChange={handleChange}
             aria-label="basic tabs example"
           >
-            <Tab label="Submissions" />
-            <Tab label="Analytics" />
-            <Tab label="Questions" />
-            <Tab label="Settings" />
+            <Tab label="Submissions" {...a11yProps(0)} />
+            <Tab label="Analytics" {...a11yProps(1)} />
+            <Tab label="Questions" {...a11yProps(2)} />
+            <Tab label="Settings" {...a11yProps(3)} />
           </Tabs>
         </Box>
+        <CustomTabPanel index={0} value={value}>
+          <FormSubmissionTable
+            formSubmissions={props.form?.submissions}
+          ></FormSubmissionTable>
+        </CustomTabPanel>
+        <CustomTabPanel index={1} value={value}></CustomTabPanel>
+        <CustomTabPanel index={2} value={value}></CustomTabPanel>
+        <CustomTabPanel index={3} value={value}></CustomTabPanel>
       </Box>
       <Button
         variant="outlined"
