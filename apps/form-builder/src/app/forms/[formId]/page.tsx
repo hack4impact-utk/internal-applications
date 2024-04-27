969f9d32640dc786fbcec5d2abd5fc34a2c10c46
Andrew Rutter <andlrutt@vols.utk.edu>
2024-04-27 19:04:49 -0400

    use typography components

--- apps/form-builder/src/app/forms/[formId]/page.tsx
+++ apps/form-builder/src/app/forms/[formId]/page.tsx
@@ -1,6 +1,7 @@
 import { getFormById } from '@/server/actions/forms';
 import * as React from 'react';
 import FormTabs from '@/components/FormTabs';
+import { Typography } from '@mui/material';
 
 export default async function FormPage({
   params,
@@ -16,12 +17,20 @@ export default async function FormPage({
   return (
     <div>
       {/* display properties of a form */}
-      <p>Title: {form.title}</p>
-      <p>Description: {form.description}</p>
-      <p>Responder Type: {form.responderType}</p>
-      <p>Anonymous: {form.isAnonymous.toLocaleString()}</p>
-      <p>Created At: {form.createdAt.toLocaleString()}</p>
-      <p>Updated At: {form.updatedAt.toLocaleString()}</p>
+      <Typography sx={{ pt: 2 }}>Title: {form.title}</Typography>
+      <Typography sx={{ pt: 2 }}>Description: {form.description}</Typography>
+      <Typography sx={{ pt: 2 }}>
+        Responder Type: {form.responderType}
+      </Typography>
+      <Typography sx={{ pt: 2 }}>
+        Anonymous: {form.isAnonymous.toLocaleString()}
+      </Typography>
+      <Typography sx={{ pt: 2 }}>
+        Created At: {form.createdAt.toLocaleString()}
+      </Typography>
+      <Typography sx={{ pt: 2 }}>
+        Updated At: {form.updatedAt.toLocaleString()}
+      </Typography>
       {/* //added tabs and a button */}
       <FormTabs form={form}></FormTabs>
     </div>
