d669810fddee7d063dbcc1b56fa2e7c839013dcb
judah benjamin <judahjosh@gmail.com>
2024-10-23 21:54:24 -0400

    modified:   apps/form-builder/src/app/forms/create/page.tsx 	modified:   package-lock.json

--- apps/form-builder/src/app/forms/create/page.tsx
+++ apps/form-builder/src/app/forms/create/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@mui/material';
+import { Card } from '@mui/material';
 export default async function NewFormPage() {
-  return <h1>New form page</h1>;
+  return (
+    <>
+      <h1>New form page</h1>
+      <button>click here</button>
+    </>
+  );
 }
