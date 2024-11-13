9e696411fad64779d4a7b771caa0a884d4eba499
judah benjamin <judahjosh@gmail.com>
2024-11-13 10:00:34 -0500

    modified:   apps/form-builder/src/components/CreateFormQuestion/index.tsx

--- apps/form-builder/src/components/CreateFormQuestion/index.tsx
+++ apps/form-builder/src/components/CreateFormQuestion/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Textfeild from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const App: React.FC = () => {
   return (
-    <div style={{ margin: '20ptx' }}>
-      <Textfeild label="Question" variant="outlined" fullWidth />
-    </div>
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20px',
+      }}
+    >
+      <div style={{ margin: '20ptx' }}>
+        <Textfeild label="Question" variant="outlined" fullWidth />
+      </div>
+    </Box>
   );
 };
 
