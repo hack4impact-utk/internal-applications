ac5d5129e380994c536694624a1029707b216a39
John Mason <johnmason5th@gmail.com>
2024-11-14 12:18:58 -0500

    GET for '[memberId]' functional, export to access 'getAllMembers'

--- apps/admin-portal/src/app/api/members/[memberId]/route.ts
+++ apps/admin-portal/src/app/api/members/[memberId]/route.ts
@@ -1,10 +1,17 @@
 import dbConnect from '@/utils/db-connect';
-import NextResponse from 'next/server';
+import { NextResponse } from 'next/server';
+import { getAllMembers } from '@hack4impact-utk/internal-models';
 
+// GET members API endpoint for getting member data
 export async function GET() {
   await dbConnect();
 
-  NextResponse.json([memberId], { status: [status code] });
+  const members = getAllMembers();
 
-  return 'OK 200';
+  // Returns 'Error' if error with accessing database
+  if (!members) {
+    return NextResponse.json({ error: 'Error' }, { status: 404 });
+  }
+
+  return NextResponse.json([members], { status: 200 });
 }
