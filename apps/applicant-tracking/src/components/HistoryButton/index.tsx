750878df98d71883616fced9704aa57eef479fc1
Tyler Cartier <46423276+TylerCartier106@users.noreply.github.com>
2025-04-02 17:27:57 -0400

    Updated HistoryButton Functionality minor change

--- apps/applicant-tracking/src/components/HistoryButton/index.tsx
+++ apps/applicant-tracking/src/components/HistoryButton/index.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 
 const HistoryButton = () =>{
     return (<Button variant='contained'
-    href='/history'>
+    href='/applicants/history'>
         History
     </Button>
     );
