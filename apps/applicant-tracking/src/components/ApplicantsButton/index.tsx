183b42ee934c437ca1fab52149718e0296047fd6
Deep0320 <dpatel03204@gmail.com>
2025-04-09 18:06:07 -0400

    finished issue #277

--- apps/applicant-tracking/src/components/ApplicantsButton/index.tsx
+++ apps/applicant-tracking/src/components/ApplicantsButton/index.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 
 const ApplicantsButton: React.FC = () => {
   return (
-    <Button variant="contained" color="primary" sx={{ borderRadius: '20px', padding: '5px 50px', backgroundColor: '#42A5F5', color:'black'}}
+    <Button variant="contained" color="primary" style={{ borderRadius: '40px', width:'400px', height:'50px', backgroundColor: '#42A5F5', color:'white', fontSize: '25px', textTransform: 'capitalize' }}
     href="/applicants"> {/* Added href component to redirect to Applicants page */}
       Applicants
     </Button>
--- apps/applicant-tracking/src/components/LBPTeamsButton/index.tsx
+++ apps/applicant-tracking/src/components/LBPTeamsButton/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Button from '@mui/material/Button';
 
 const LBPButton = () => {
-  return <Button variant="contained">LBP Teams</Button>;
+  // return <Button variant="contained">LBP Teams</Button>;
+  return( 
+    <Button variant="contained" color="primary" style={{ borderRadius: '40px', width:'400px', height:'50px', backgroundColor: '#42A5F5', color:'white', fontSize: '25px', textTransform: 'capitalize' }}>
+      {/* href="/...">  */}
+        LBP Teams
+      </Button>)
 };
 
 export default LBPButton;
