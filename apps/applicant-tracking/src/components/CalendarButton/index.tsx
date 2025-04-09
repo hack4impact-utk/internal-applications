adfd8076fa0ba14cce68d066d991ca997852d82a
Tyler Cartier <46423276+TylerCartier106@users.noreply.github.com>
2025-04-09 17:30:16 -0400

    Calendar Button Implementation

---
+++ apps/applicant-tracking/src/components/CalendarButton/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const CalendarButton = () =>{
+    return (<Button variant='contained'
+    href='/applicants/calendar'>
+        Calendar
+    </Button>
+    );
+};
+
+export default CalendarButton;
