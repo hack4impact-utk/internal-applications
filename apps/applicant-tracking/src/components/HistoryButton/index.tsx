59403e0f7b3f627b2ff8aab89ae2040d92bf7df4
Braden Wingfield <wingfieldbraden@gmail.com>
2025-04-10 10:38:04 -0400

    styling and functionality added

--- apps/applicant-tracking/src/components/HistoryButton/index.tsx
+++ apps/applicant-tracking/src/components/HistoryButton/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 
-const HistoryButton = () =>{
-    return (<Button variant='contained'
-    color="primary" style={{ borderRadius: '40px', width:'400px', height:'50px', backgroundColor: '#42A5F5', color:'white', fontSize: '25px', textTransform: 'capitalize' }}
-    href='/applicants/history'>
-        History
+const HistoryButton = () => {
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      style={{
+        borderRadius: '40px',
+        width: 'min(400px, 30vw)',
+        height: 'min(50px, 6vh)',
+        backgroundColor: '#42A5F5',
+        color: 'white',
+        fontSize: 'min(25px, 2vw)',
+        textTransform: 'capitalize',
+      }}
+      href="/applicants/history"
+    >
+      History
     </Button>
-    );
+  );
 };
 
 export default HistoryButton;
