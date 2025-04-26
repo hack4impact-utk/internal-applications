55a9a4dab9f0894c71d1c46a03ed08006ccdf905
Koushik <koushiksanjays@gmail.com>
2025-04-26 14:29:20 -0400

    Made some updates

--- apps/applicant-tracking/src/app/applicants/history/page.tsx
+++ apps/applicant-tracking/src/app/applicants/history/page.tsx
@@ -4,6 +4,8 @@ import { LineChart } from '@mui/x-charts/LineChart';
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box';
 import { Circle } from '@mui/icons-material';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 
 const data = [{date: '2023', ap: 50, ac: 25, ret: 10}, {date: '2024', ap: 30, ac: 70, ret: 60}, {date: '2025', ap: 80, ac: 60, ret: 30}];
 const graphKeys = {
@@ -27,6 +29,67 @@ export default function HistoryPage() {
     <>
     <Typography variant='h4' align='center' style={{padding: '10px'}}>History</Typography>
     <Box sx={{ width: '95%', height: 5, bgcolor: 'orange', margin: 'auto'}} />
+
+        <Box sx={{ 
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               marginTop: 4,
+               marginBottom: 6,
+               marginRight: '1%',
+               // Giving space betweent the 2 filters
+               gap: 5,
+               }}>
+        
+              <FormControl sx = {{
+                minWidth: 150,
+                border: '2px solid',
+                borderColor: 'grey.700',
+                borderRadius: 1,
+                backgroundColor: 'white',
+                justifyContent: 'center',
+              }}>
+                <NativeSelect
+        
+                  defaultValue={1}
+        
+                  inputProps={{
+                    id: 'uncontrolled-native',
+                  }}
+        
+                >     
+                {/* The content inside the filters like dates could be updated in the future
+                depending upon the need */}
+                  <option value={1}>Fall 2023</option>
+                  <option value={2}>Fall 2024</option>
+                </NativeSelect>
+              </FormControl>
+                <Typography  sx={{mx: 0.5 }}> To </Typography>
+              <FormControl sx = {{
+                minWidth: 150,
+                border: '2px solid',
+                borderColor: 'grey.700',
+                borderRadius: 1,
+                backgroundColor: 'white',
+                justifyContent: 'center',
+              }}>
+                <NativeSelect
+        
+                  defaultValue={2}
+        
+                  inputProps={{
+                    id: 'uncontrolled-native',
+                  }}
+        
+                >
+               {/* The content inside the filters like dates could be updated in the future
+                depending upon the need */}
+                  <option value={1}>Fall 2023</option>
+                  <option value={2}>Fall 2024</option>
+        
+                </NativeSelect>
+              </FormControl>
+            </Box>
 
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       <Button variant='contained' startIcon={<Circle style={{fill: '#72b8cc'}}/>} sx={graphKeys}>Applications</Button>
